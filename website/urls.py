d4881d51936d758ba0b07287fff11669f52a58be
Natalya <nakorgutlova@stud.kpfu.ru>
2019-11-24 11:50:38 +0300

    small fix

--- website/urls.py
+++ website/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url('edit_profile/', update_profile, name='update_profile'),
     url('create_definition/', page_create_definition, name='page_create_definition'),
     url('definition/', page_create_definition, name='create_definition'),
-    url(r'^definition/(?P<id>\d+)$', definition, name='definition')
+    url(r'^definition/(?P<id>\d+)$', definition, name='definition'),
     url('activate_user/', activate_user, name='activate_user'),
 ]
