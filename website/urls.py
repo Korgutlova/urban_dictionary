b6259ddb2449385123484000b13f7bbcc7c6561e
Amir <thelinkod@mail.ru>
2019-11-30 14:18:59 +0300

    Merge and html fixes

--- website/urls.py
+++ website/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^$', main_page, name="main_page"),
     url(r'^edit_profile/$', update_profile, name='update_profile'),
     url(r'^create_definition/$', page_create_definition, name='page_create_definition'),
-    url(r'^definition/(?P<pk>\d+)$',  TermView.as_view(), name='definition'),
+    url(r'^definition/<int:pk>$',  TermView.as_view(), name='definition'),
     url(r'^activate_user/$', activate_user, name='activate_user'),
 ]
