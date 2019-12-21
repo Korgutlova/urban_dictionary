dac1b47df033aa126b1a4e110688cda8d5ddf120
KadyrovAmir <amir.kadyrov.97@mail.ru>
2019-12-21 14:51:36 +0300

    fix for static files

--- urban_dictionary/settings.py
+++ urban_dictionary/settings.py
@@ -126,7 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+else:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
